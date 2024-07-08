48ba47e448d48c04050c5913707fec6d750cd224
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-07-08 11:43:35 +0300

    add proxy for images

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import 'dotenv/config';
 
 const app = express();
 
-const proxy = createProxyMiddleware({
+const apiProxy = createProxyMiddleware({
   target: 'https://api.themoviedb.org',
   changeOrigin: true,
 });
 
-app.use(proxy);
+const imgProxy = createProxyMiddleware({
+  target: 'https://image.tmdb.org',
+  changeOrigin: true,
+});
+
+app.use('/api', apiProxy);
+
+app.use('/img', imgProxy);
 
 app.listen(process.env.PORT || 3000);
